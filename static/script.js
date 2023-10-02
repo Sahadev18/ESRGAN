cab1a33152cc47e3cbde7c42989e58045fc1ddf4
Sahadev Bhaganagare <76152788+Sahadev18@users.noreply.github.com>
2023-10-02 17:31:14 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -8,12 +8,19 @@ function displayImage() {
   // Set any additional attributes you need (e.g., width, height)
   img.setAttribute('width', '200');
 
-  // Get the container div to display the image
+  // Create a download button
+  var downloadBtn = document.createElement('a');
+  downloadBtn.href = './static/SR.jpg';  // Replace with the actual path to your image
+  downloadBtn.download = 'SR.jpg';  // Specify the desired download filename
+  downloadBtn.innerText = 'Download Image';
+
+  // Get the container div to display the image and the download button
   var imageContainer = document.getElementById('imageContainer');
 
   // Clear previous content in the container
   imageContainer.innerHTML = '';
 
-  // Append the image to the container
+  // Append the image and download button to the container
   imageContainer.appendChild(img);
+  imageContainer.appendChild(downloadBtn);
 }
